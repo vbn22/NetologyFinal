a14d9cc76ca2faaa7b923d020c3dfbc0d275a7de
michael <v.b.n@mail.ru>
2016-12-23 14:10:09 +0300

    Test client register

--- main/models.py
+++ main/models.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
+class Client(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,blank=True,null=True,related_name='profile')
+    wallet = models.FloatField(default=100.0, verbose_name='price')
 
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Client.objects.create(user=instance)
--- main/tests.py
+++ main/tests.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
 import unittest
+from django.contrib.auth.models import User
+from django.test import Client as TestClient
 
-class TestBasic(unittest.TestCase):
-    "Basic tests"
 
-    def test_basic(self):
-        a = 1
-        self.assertEqual(1, a)
+class BasicTest(unittest.TestCase):
 
-    def test_basic_2(self):
-        a = 1
-        assert a == 1
+    def test_client_can_register(self):
+        self.client = TestClient()
+        email = 'email@email.ru'
+        data = dict(username='testuser',wallet=100,last_name='last_name',email=email)
+        self.client.post('/register/',data)
+        print User.objects.filter(email=email)
+        self.assertTrue(User.objects.filter(email=email))
 
